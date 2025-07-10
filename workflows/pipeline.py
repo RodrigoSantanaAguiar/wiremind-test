ab05771b0145f4e12477a1022f3964baa8259115
RodrigoSantanaAguiar <166901536+RodrigoSantanaAguiar@users.noreply.github.com>
2025-07-10 10:40:42 +0200

    define prepare_data step

--- workflows/pipeline.py
+++ workflows/pipeline.py
@@ -18,3 +18,28 @@ global_config.image = IMAGE_NAME
 global_config.image_pull_policy = ImagePullPolicy.never
 
 
+@script()
+def prepare_data(data_path: str) -> dict:
+    """
+    Load data and filter confirmed tickets.
+    :param data_path: The path to the parquet file.
+    :return: A dictionary with the filtered data and a list of services.
+    """
+
+    # Import module here because this code will execute inside a container
+    from data_pipeline.process import load_data, filter_confirmed_tickets
+    import logging
+    import json
+
+    logging.info(f"Loading data from {data_path}...")
+    raw_df = load_data(data_path)
+    filtered_df = filter_confirmed_tickets(raw_df)
+    services = filtered_df["service_number"].unique().tolist()
+
+    logging.info(f"Loaded {filtered_df.shape[0]} confirmed tickets.")
+    return {
+        "filtered_data": filtered_df,   # Hera saves dataFrames as an artefact (Parquet)
+        "services_list": services,      # Hera saves lists as a parameter (JSON)
+    }
+
+
